10831a25b41bfe321e8c9461150ffa076776e7f7
Administrator <Administrator@DESKTOP-099UQRG>
2020-10-20 16:42:42 -0400

    Creating model classes and constructers for Employees

---
+++ src/main/java/com/hrplus/model/MarketingEmp.java
@@ -0,0 +1,92 @@
+package com.hrplus.model;
+
+public class MarketingEmp {
+	
+	private String id;
+	private String name;
+	private String salary;
+	private String vacaAvailable;
+	private String vacaTaken;
+	private String vacaLeft;
+	private String investments;
+	
+	public MarketingEmp() {
+		
+	}
+
+	public MarketingEmp(String id, String name, String salary, String vacaAvailable, String vacaTaken, String vacaLeft,
+			String investments) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.vacaAvailable = vacaAvailable;
+		this.vacaTaken = vacaTaken;
+		this.vacaLeft = vacaLeft;
+		this.investments = investments;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSalary() {
+		return salary;
+	}
+
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+
+	public String getVacaAvailable() {
+		return vacaAvailable;
+	}
+
+	public void setVacaAvailable(String vacaAvailable) {
+		this.vacaAvailable = vacaAvailable;
+	}
+
+	public String getVacaTaken() {
+		return vacaTaken;
+	}
+
+	public void setVacaTaken(String vacaTaken) {
+		this.vacaTaken = vacaTaken;
+	}
+
+	public String getVacaLeft() {
+		return vacaLeft;
+	}
+
+	public void setVacaLeft(String vacaLeft) {
+		this.vacaLeft = vacaLeft;
+	}
+
+	public String getInvestments() {
+		return investments;
+	}
+
+	public void setInvestments(String investments) {
+		this.investments = investments;
+	}
+
+	@Override
+	public String toString() {
+		return "MarketingEmp [id=" + id + ", name=" + name + ", salary=" + salary + ", vacaAvailable=" + vacaAvailable
+				+ ", vacaTaken=" + vacaTaken + ", vacaLeft=" + vacaLeft + ", investments=" + investments + "]";
+	}
+	
+	
+
+}
