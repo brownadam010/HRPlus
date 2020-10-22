26d4a4a96695170c6676e04f04caf02acc9d4733
Administrator <Administrator@DESKTOP-099UQRG>
2020-10-21 21:38:37 -0400

    Application.java to HRPlusApplication.java refractor

--- src/main/java/com/hrplus/HRPlusApplication.java
+++ src/main/java/com/hrplus/HRPlusApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class HRPlusApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(HRPlusApplication.class, args);
 	}
 
 }
