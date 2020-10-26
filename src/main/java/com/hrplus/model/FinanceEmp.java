a16f3cc33400ffed7fd27c724500a3fa0a30a382
Trent McGinnis <46269691+TrentMcGinnis@users.noreply.github.com>
2020-10-26 00:02:26 -0400

    forgot to remove

--- src/main/java/com/hrplus/model/FinanceEmp.java
+++ src/main/java/com/hrplus/model/FinanceEmp.java
@@ -5,13 +5,6 @@ public class FinanceEmp extends Employee {
 	public FinanceEmp(int id, String name, String salary, String vacaAvailable, String vacaTaken, String vacaLeft,
 			String investments) {
 				super(id, name, salary, vacaAvailable, vacaTaken, vacaLeft, investments);
-		this.id = id;
-		this.name = name;
-		this.salary = salary;
-		this.vacaAvailable = vacaAvailable;
-		this.vacaTaken = vacaTaken;
-		this.vacaLeft = vacaLeft;
-		this.investments = investments;
 	}
 
 	@Override
